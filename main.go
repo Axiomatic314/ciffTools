ef879f8a4cc6bec6d16745d226d15f1d2ffa3047
Axiomatic314 <rumkate289@gmail.com>
2024-09-25 15:00:53 +1200

    reads in a CIFF

--- main.go
+++ main.go
@@ -1,20 +1,94 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
+	"flag"
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/Axiomatic314/qCIFF/ciff"
 	"google.golang.org/protobuf/proto"
 )
 
+func ReadNextMessage(bufferedReader *bufio.Reader, messageStruct proto.Message) error {
+	sizeBuffer, err := bufferedReader.Peek(10)
+	if err != nil {
+		slog.Debug("error trying to peek at message length", "error", err)
+	}
+	messageSize, bytesRead := binary.Uvarint(sizeBuffer)
+	// fmt.Printf("%d, %d\n", messageSize, bytesRead)
+	bufferedReader.Discard(bytesRead)
+
+	byteBuffer := make([]byte, messageSize)
+	_, err = bufferedReader.Read(byteBuffer)
+	if err != nil {
+		slog.Debug("error reading message bytes", "error", err)
+		return err
+	}
+
+	err = proto.Unmarshal(byteBuffer, messageStruct)
+	if err != nil {
+		slog.Debug("error during unmarshal", "error", err)
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-	filename := "test-complete.ciff"
-	in, _ := os.ReadFile(filename)
+
+	ciffFilePath := flag.String("ciffFilePath", "", "filepath of CIFF file to read in")
+	flag.Parse()
+
+	ciffFileHandle, err := os.Open(*ciffFilePath)
+	if err != nil {
+		slog.Error("error opening ciff", "error", err)
+		os.Exit(1)
+	}
+
+	ciffReader := bufio.NewReader(ciffFileHandle)
+
+	// --------------------------------------------------------------------------------
+	// Header
+	slog.Debug("reading header")
+
 	header := &ciff.Header{}
-	// n, t := protowire.ConsumeTag(int)
-	// fmt.Printf("%d, %d\n", n, t)
-	if err := proto.Unmarshal(in, header); err != nil {
-		fmt.Println("Failed to parse header:", err)
+	err = ReadNextMessage(ciffReader, header)
+	if err != nil {
+		slog.Error("error reading header message", "error", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Header: %+v\n", header)
+
+	// --------------------------------------------------------------------------------
+	// PostingsList
+	slog.Debug("reading postings lists")
+
+	postingsListSlice := make([]*ciff.PostingsList, header.NumPostingsLists)
+	for postingsListIndex := range header.NumPostingsLists {
+		postingsList := &ciff.PostingsList{}
+		ReadNextMessage(ciffReader, postingsList)
+		postingsListSlice[postingsListIndex] = postingsList
+		slog.Debug("postingsList decoded", "index", postingsListIndex, "postingsList", postingsListSlice[postingsListIndex])
+	}
+	for postingsListIndex, postingsList := range postingsListSlice {
+		fmt.Printf("Postings List %v: %+v\n", postingsListIndex, postingsList)
+	}
+
+	// --------------------------------------------------------------------------------
+	// DocRecord
+	slog.Debug("reading doc records")
+
+	docRecordSlice := make([]*ciff.DocRecord, header.NumDocs)
+	for docRecordIndex := range header.NumDocs {
+		r := &ciff.DocRecord{}
+		ReadNextMessage(ciffReader, r)
+		docRecordSlice[docRecordIndex] = r
+		slog.Debug("docRecord decoded", "index", docRecordIndex, "docRecord", docRecordSlice[docRecordIndex])
+	}
+	for docRecordIndex, docRecord := range docRecordSlice {
+		fmt.Printf("Doc Record %v: %+v\n", docRecordIndex, docRecord)
 	}
 }
